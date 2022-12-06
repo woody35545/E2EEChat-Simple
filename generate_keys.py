7fe7954d8ed42965b344d883fb3ced0eb080dc32
Woody_K <84436996+woody35545@users.noreply.github.com>
2022-12-06 13:34:40 +0900

    Create generate_keys.py

---
+++ generate_keys.py
@@ -0,0 +1,27 @@
+from Crypto import Random
+from Crypto.PublicKey import RSA
+import base64
+
+def encode_base64(p):
+    return base64.b64encode(p).decode('ascii')
+
+# 32바이트 (256비트) 랜덤 비밀키 생성
+secret = Random.get_random_bytes(32)
+
+rsa = RSA.generate(2048) # RSA 2048 키 생성 시작
+
+pubkey = rsa.public_key() # 공개키 export
+# export 공개키 저장
+f = open("pubkey.key",'wb')
+f.write(pubkey.exportKey('PEM'))#base64로 인코딩된 것을 넣기
+f.close()
+
+prikey = rsa.exportKey('PEM') # 개인키 export
+f = open("prikey.key",'wb')
+f.write(prikey.exportKey('PEM'))
+f.close()
+
+#print(encode_base64(secret) + '\n')
+
+#print(encode_base64(pubkey) + '\n')
+#print(encode_base64(prikey) + '\n')
